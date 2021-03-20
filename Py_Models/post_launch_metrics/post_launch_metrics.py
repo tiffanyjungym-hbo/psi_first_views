e1e6439d260c46cb1d3636fb3e986dd9cc522522
jonnyhbo <70723253+jonnyhbo@users.noreply.github.com>
2021-03-19 17:19:55 -0700

    Add metric query (#13)

--- Py_Models/post_launch_metrics/post_launch_metrics.py
+++ Py_Models/post_launch_metrics/post_launch_metrics.py
@@ -6,20 +6,33 @@ import argparse
 import logging
 import pandas as pd
 import pathlib
+import time
 
 from airflow.models import Variable
 from common import snowflake_utils
 
 SNOWFLAKE_ACCOUNT_NAME = Variable.get('SNOWFLAKE_ACCOUNT_NAME')  # 'hbomax.us-east-1'
 QUERY_SUBSCRIBER_TABLE = 'total_sub_base_table.sql'
-QUERY_METRIC_TABLE = ''
 CURRENT_PATH = pathlib.Path(__file__).parent.absolute()
+QUERY_FUNNEL_METRICS = 'title_retail_funnel_metrics_update'
+DAY_LIST = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 28]  # [ndays] since first offered
+PLATFORM_LIST = ['hboMax', 'hboNow']
+DAY_LATENCY = 0  # started counting after [day_latency] days
+STREAM_TABLE = {'hboMax':"'max_prod.viewership.max_user_stream_heartbeat_view'", 'hboNow':"'max_prod.viewership.now_user_stream'"}  # source of viewership, either heartbeat or now_uer_stream
+END_DATE = {'hboNow':"'2020-05-27'", 'hboMax':"'2021-03-02'"}  # the end date of the viewership data
+EXIST_IND_VAL = 0  # indicating if a title_name - platform_name - days_since_first_offered combination exists in the target table
 
 logger = logging.getLogger()
 
 def execute_query(query: str, database :str, schema: str, warehouse: str, role: str, snowflake_env: str) -> pd.DataFrame:
 	"""
 	Execute a query on snowflake
+
+	:param database: name of the database
+	:param schema: name of the schema
+	:param warehouse: name of the warehouse
+	:param role: name of the role
+	:param snowflake_env: environment used in Snowflake
 	"""
 	connection = snowflake_utils.connect(
 		  SNOWFLAKE_ACCOUNT_NAME,
@@ -40,6 +53,8 @@ def execute_query(query: str, database :str, schema: str, warehouse: str, role: 
 def load_query(filename: str, **kwargs) -> str:
 	"""
 	Load a query from disk and fill templates parameters
+
+	:param filename: name of the file that contains the query
 	"""
 	with open(filename, 'r') as f:
 		query = f.read()
@@ -47,32 +62,126 @@ def load_query(filename: str, **kwargs) -> str:
 	query = query.format(**kwargs)
 	return query
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--SNOWFLAKE_ENV', required=True)
-parser.add_argument('--WAREHOUSE', required=True)
-parser.add_argument('--ROLE', required=True)
-parser.add_argument('--DATABASE', required=True)
-parser.add_argument('--SCHEMA', required=True)
-args = parser.parse_args()
+def update_subscriber_table(
+	database: str,
+	schema: str,
+	warehouse: str,
+	role: str,
+	snowflake_env: str
+) -> pd.DataFrame:
+	"""
+	Update the denominator table for content funnel metrics
 
-logger.info(f'Loading query {QUERY_SUBSCRIBER_TABLE}')
-query_subscriber_table = load_query(f'{CURRENT_PATH}/{QUERY_SUBSCRIBER_TABLE}', database=args.DATABASE, schema=args.SCHEMA)
+	:param database: name of the database
+	:param schema: name of the schema
+	:param warehouse: name of the warehouse
+	:param role: name of the role
+	:param snowflake_env: environment used in Snowflake
+	"""
+	logger.info(f'Loading query {QUERY_SUBSCRIBER_TABLE}')
 
-logger.info(f'{query_subscriber_table[:100]}')
+	query_subscriber_table = load_query(QUERY_SUBSCRIBER_TABLE, database=database, schema=schema)
 
-logger.info('Executing query using environment:')
-logger.info(f'snowflake env: {args.SNOWFLAKE_ENV}')
-logger.info(f'warehouse: {args.WAREHOUSE}')
-logger.info(f'role: {args.ROLE}')
-logger.info(f'database: {args.DATABASE}')
-logger.info(f'schema: {args.SCHEMA}')
+	df_subscriber_table = execute_query(
+		query=query_subscriber_table,
+		database=database,
+		schema=schema,
+		warehouse=warehouse,
+		role=role,
+		snowflake_env=snowflake_env
+	)
+	logger.info(f'Query returned shape: {df_subscriber_table.shape}')
 
-df_subscriber_table = execute_query(
-	query=query_subscriber_table,
-	database=args.DATABASE,
-	schema=args.SCHEMA,
-	warehouse=args.WAREHOUSE,
-	role=args.ROLE,
-	snowflake_env=args.SNOWFLAKE_ENV
-)
-logger.info(f'Query returned shape: {df_subscriber_table.shape}')
+	return df_subscriber_table
+
+def update_funnel_metrics_table(
+	database: str,
+	schema: str,
+	warehouse: str,
+	role: str,
+	snowflake_env: str
+) -> pd.DataFrame:
+	"""
+	Update the numerator table for content funnel metrics
+
+	:param database: name of the database
+	:param schema: name of the schema
+	:param warehouse: name of the warehouse
+	:param role: name of the role
+	:param snowflake_env: environment used in Snowflake
+	"""
+	# Create latest funnel metrics
+	logger.info(f'Loading query {QUERY_FUNNEL_METRICS}')
+
+	df_funnel_metrics = pd.DataFrame()
+
+	for nday in DAY_LIST:
+		for platform in PLATFORM_LIST:
+
+			logger.info(f'Getting data for nth day: {nday} on {platform}')
+
+			query_funnel_metrics = load_query(
+				QUERY_FUNNEL_METRICS,
+				database=database,
+				schema=schema,
+				nday=nday,
+				day_latency=DAY_LATENCY,
+				stream_table=STREAM_TABLE[platform],
+				end_date=END_DATE[platform],
+				exist_ind_val=EXIST_IND_VAL
+			)
+
+			start_time = time.time()
+
+			_df_funnel_metrics = execute_query(
+				query=query_funnel_metrics,
+				database=database,
+				schema=schema,
+				warehouse=warehouse,
+				role=role,
+				snowflake_env=snowflake_env
+			)
+
+			end_time = time.time()
+			logger.info(f'Time taken {end_time - start_time} seconds')
+
+			df_funnel_metrics = pd.concat([df_funnel_metrics, _df_funnel_metrics], axis=0)
+
+	return df_funnel_metrics
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--SNOWFLAKE_ENV', required=True)
+	parser.add_argument('--WAREHOUSE', required=True)
+	parser.add_argument('--ROLE', required=True)
+	parser.add_argument('--DATABASE', required=True)
+	parser.add_argument('--SCHEMA', required=True)
+	args = parser.parse_args()
+
+	logger.info('Environment defined:')
+	logger.info(f'snowflake env: {args.SNOWFLAKE_ENV}')
+	logger.info(f'warehouse: {args.WAREHOUSE}')
+	logger.info(f'role: {args.ROLE}')
+	logger.info(f'database: {args.DATABASE}')
+	logger.info(f'schema: {args.SCHEMA}')
+
+	logger.info('Updating subcriber table')
+	df_subscriber_table = update_subscriber_table(
+		database=args.DATABASE,
+		schema=args.SCHEMA,
+		warehouse=args.WAREHOUSE,
+		role=args.ROLE,
+		snowflake_env=args.SNOWFLAKE_ENV
+	)
+
+	logger.info('Updating metrics table')
+	df_funnel_metrics = update_funnel_metrics_table(
+		database=args.DATABASE,
+		schema=args.SCHEMA,
+		warehouse=args.WAREHOUSE,
+		role=args.ROLE,
+		snowflake_env=args.SNOWFLAKE_ENV
+	)
+
+	logger.info('Finished table updates')

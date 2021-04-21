a91a19377adc0e3456ae48494e178df0f95d82ec
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-04-21 15:30:09 -0400

    Dev test 2 (#85)
    * update
    
    * hotfix/trailer feature .py
    
    * trailer update

--- Py_Models/prediction_feature_extraction/prediction_feature_extraction.py
+++ Py_Models/prediction_feature_extraction/prediction_feature_extraction.py
@@ -130,7 +130,7 @@ def run_feature_query_list(
 		end_time = time.time()
 		logger.info(f'Time taken {end_time - start_time} seconds')
  
-		feature_data = pd.concat([feature_data, feature_data], axis=1)
+        feature_data = pd.concat([feature_data, feature_data], axis=1)
          
 	return feature_data
 

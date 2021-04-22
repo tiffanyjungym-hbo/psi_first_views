ad34019c92743fde399f2d3678e561cf30d75733
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-04-22 12:14:22 -0400

    Dev test (#99)
    * update
    
    * hotfix/trailer feature .py
    
    * trailer update
    
    * trailersql
    
    * sql extraction
    
    * sql extraction
    
    * update
    
    * layout
    
    * msg
    
    * update dir
    
    * sql
    
    * msg
    
    * msg

--- Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_trailer.sql
+++ Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_trailer.sql
@@ -4,8 +4,8 @@ FROM (
           concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id) as match_id_platform
         , ifnull(retail_trailer_view_metric, -1) as retail_trailer_view_metric
-    from {schema}.trailer_view_percent_test as t
-    right join {schema}.title_retail_funnel_metrics as f
+    from max_beta.{schema}.trailer_view_percent_test as t
+    right join max_beta.{schema}.title_retail_funnel_metrics as f
         on t.match_id_platform = concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id)
     order by retail_trailer_view_metric desc)

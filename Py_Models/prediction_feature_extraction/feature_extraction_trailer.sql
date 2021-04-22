161d691cd4bb369f29e8f861f7d7b0a2ce28a292
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-04-22 11:17:18 -0400

    Dev test (#93)
    * update
    
    * hotfix/trailer feature .py
    
    * trailer update
    
    * trailersql
    
    * sql extraction
    
    * sql extraction
    
    * update

--- Py_Models/prediction_feature_extraction/feature_extraction_trailer.sql
+++ Py_Models/prediction_feature_extraction/feature_extraction_trailer.sql
@@ -4,13 +4,14 @@ FROM (
           concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id) as match_id_platform
         , ifnull(retail_trailer_view_metric, -1) as retail_trailer_view_metric
-    from max_dev.workspace.trailer_view_percent_test as t
-    right join max_dev.workspace.title_retail_funnel_metrics as f
+    from {schema}.trailer_view_percent_test as t
+    right join {schema}.title_retail_funnel_metrics as f
         on t.match_id_platform = concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id)
     order by retail_trailer_view_metric desc)
 
     select * from final
+    order by
 ) file_format = (type='csv') OVERWRITE = TRUE header = TRUE;
 
 ;

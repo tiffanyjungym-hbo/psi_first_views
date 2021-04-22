9594a2d26dc73bea08a47ae3552aec9fdc511bba
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-04-22 11:02:35 -0400

    Dev test (#92)
    * update
    
    * hotfix/trailer feature .py
    
    * trailer update
    
    * trailersql
    
    * sql extraction
    
    * sql extraction

--- Py_Models/prediction_feature_extraction/feature_extraction_trailer.sql
+++ Py_Models/prediction_feature_extraction/feature_extraction_trailer.sql
@@ -4,8 +4,8 @@ FROM (
           concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id) as match_id_platform
         , ifnull(retail_trailer_view_metric, -1) as retail_trailer_view_metric
-    from {database}.{schema}.{trailer_table} as t
-    right join {database}.{schema}.{funnel_metrics_table} as f
+    from max_dev.workspace.trailer_view_percent_test as t
+    right join max_dev.workspace.title_retail_funnel_metrics as f
         on t.match_id_platform = concat(case when f.platform_name = 'hboNow' then 0
                     else 1 end, '-', f.match_id)
     order by retail_trailer_view_metric desc)

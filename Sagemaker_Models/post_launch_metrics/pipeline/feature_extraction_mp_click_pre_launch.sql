4c2c51a617da756f9d48c2719789d893c2657a6d
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-04-26 16:26:22 -0400

    Dev test (#108)
    * update
    
    * hotfix/trailer feature .py
    
    * trailer update
    
    * trailersql
    
    * sql extraction
    
    * sql extraction
    
    * update
    
    * layout
    
    * msg
    
    * update dir
    
    * sql
    
    * msg
    
    * msg
    
    * msg
    
    * trailer sql hotfix
    
    * trailer sql update
    
    * sql update
    
    * sql update
    
    * update
    
    * sql
    
    * sqls
    
    * content cost

--- Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_mp_click_pre_launch.sql
+++ Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_mp_click_pre_launch.sql
@@ -1,61 +1,66 @@
-with title_id as (
-    select distinct
-          title_name
-        , platform_name
-        , earliest_offered_timestamp
-        , concat(case when f.platform_name = 'hboNow' then 0
-                else 1 end, '-', f.match_id) as match_id_platform
-    from {database}.{schema}.{funnel_metrics_table} as f
-),
+USE SCHEMA workspace;
 
-click_value as (select
-      case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
-          then custom_dimension_11 else originals_series_calc
-              end as originals_series_calc
-    , t.match_id_platform
-    , ln(sum(clicks+1)) as ln_total_click_from_marketing_spend
-from title_id as t
-left join max_prod.analytics.marketing_datorama_in_platform_attr_v2 as am
-    on lower(case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
-          then custom_dimension_11 else originals_series_calc end) = lower(get(split(t.title_name, ' S'),0))
-        and case when hbo_product_calc = 'MAX' then 'hboMax'
-            else 'hboNow' end = platform_name
-where 1=1
-    and hbo_product_calc in ('MAX', 'HBO')
-    and day between dateadd(day, -56, t.earliest_offered_timestamp)
-        and dateadd(min, -1, t.earliest_offered_timestamp)
-group by 1,2 ),
+COPY INTO {stage}/mp_click_prelaunch_feature.csv
+FROM (
+    with title_id as (
+        select distinct
+            title_name
+            , platform_name
+            , earliest_offered_timestamp
+            , concat(case when f.platform_name = 'hboNow' then 0
+                    else 1 end, '-', f.match_id) as match_id_platform
+        from {database}.{schema}.title_retail_funnel_metrics as f
+    ),
 
-title_count as (
-    select
+    click_value as (select
         case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
-          then custom_dimension_11 else originals_series_calc
-              end as originals_series_calc
-        , count(distinct match_id_platform) as title_count
-    from title_id as f
+            then custom_dimension_11 else originals_series_calc
+                end as originals_series_calc
+        , t.match_id_platform
+        , ln(sum(clicks+1)) as ln_total_click_from_marketing_spend
+    from title_id as t
     left join max_prod.analytics.marketing_datorama_in_platform_attr_v2 as am
         on lower(case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
-          then custom_dimension_11 else originals_series_calc end) = lower(get(split(f.title_name, ' S'),0))
-        and case when hbo_product_calc = 'MAX' then 'hboMax'
-            else 'hboNow' end = platform_name
+            then custom_dimension_11 else originals_series_calc end) = lower(get(split(t.title_name, ' S'),0))
+            and case when hbo_product_calc = 'MAX' then 'hboMax'
+                else 'hboNow' end = platform_name
     where 1=1
         and hbo_product_calc in ('MAX', 'HBO')
-        and day between dateadd(day, -56, f.earliest_offered_timestamp)
-            and dateadd(min, -1, f.earliest_offered_timestamp)
-    group by  1
-),
+        and day between dateadd(day, -56, t.earliest_offered_timestamp)
+            and dateadd(min, -1, t.earliest_offered_timestamp)
+    group by 1,2 ),
 
-final as (select
-      t.match_id_platform
-    , sum(ifnull(ln_total_click_from_marketing_spend/title_count,-1))
-        as ln_total_click_from_marketing_spend
-from title_id as t
-left join click_value as c
-    on t.match_id_platform = c.match_id_platform
-left join title_count as cnt
-    on c.originals_series_calc = cnt.originals_series_calc
-group by 1
-order by ln_total_click_from_marketing_spend desc
-)
+    title_count as (
+        select
+            case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
+            then custom_dimension_11 else originals_series_calc
+                end as originals_series_calc
+            , count(distinct match_id_platform) as title_count
+        from title_id as f
+        left join max_prod.analytics.marketing_datorama_in_platform_attr_v2 as am
+            on lower(case when originals_series_calc in ('HBO MAX GROWTH','HBO NOW')
+            then custom_dimension_11 else originals_series_calc end) = lower(get(split(f.title_name, ' S'),0))
+            and case when hbo_product_calc = 'MAX' then 'hboMax'
+                else 'hboNow' end = platform_name
+        where 1=1
+            and hbo_product_calc in ('MAX', 'HBO')
+            and day between dateadd(day, -56, f.earliest_offered_timestamp)
+                and dateadd(min, -1, f.earliest_offered_timestamp)
+        group by  1
+    ),
 
-select * from final;
+    final as (select
+        t.match_id_platform
+        , sum(ifnull(ln_total_click_from_marketing_spend/title_count,-1))
+            as ln_total_click_from_marketing_spend
+    from title_id as t
+    left join click_value as c
+        on t.match_id_platform = c.match_id_platform
+    left join title_count as cnt
+        on c.originals_series_calc = cnt.originals_series_calc
+    group by 1
+    order by ln_total_click_from_marketing_spend desc
+    )
+
+    select * from final
+) file_format = (type='csv' compression = 'NONE') single = true OVERWRITE = TRUE header = TRUE;

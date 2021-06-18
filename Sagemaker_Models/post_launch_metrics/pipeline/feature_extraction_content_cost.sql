2b93b08401f120d0b2a146e3cf59e6341b7edbe3
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-06-18 14:05:59 -0400

    content cost added (#246)

--- Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_content_cost.sql
+++ Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_content_cost.sql
@@ -2,60 +2,21 @@ use schema workspace;
 
 COPY INTO {stage}/metadata_feature.csv
 FROM (
-    with originals as (
-        select distinct
-            concat(mapped_id, case when rad.content_category = 'series'
-                then concat('-',to_char(ifnull(season, '1'))) else '' end) as match_id
-            , to_double(replace(efc, '$', '')) as cost
-        from max_dev.content_revenue.hbo_op_cd as h
-        left join max_dev.content_revenue.cd_title_map as t
-            on h.program = t.base_title
-        left join max_prod.catalog.reporting_asset_dim as rad
-            on mapped_id = rad.title_id
-    ),
+    with cost_base as (
+    select
+        concat(title_id, case when c.content_category = 'series'
+            then concat('-',to_char(ifnull(right(season_or_movie_title, 1), '1'))) else '' end) as match_id
+        , total_cost/1000000 as content_cost
+    from  max_dev.content_revenue.content_efficiency_main as c
+    )
 
-    acquired as (
-        select distinct
-            concat(mapped_id, case when rad.content_category = 'series'
-                then concat('-',to_char(ifnull(h.season_number, '1'))) else '' end) as match_id
-            , cost/1000 as cost
-        from max_dev.content_revenue.hbomax_cd as h
-        left join max_dev.content_revenue.cd_title_map as t
-            on h.show_name = t.base_title
-        left join max_prod.catalog.reporting_asset_dim as rad
-            on mapped_id = rad.title_id
-    ),
-
-    table_union as (
-        select
-            *
-        from originals
-        union
-        select
-            *
-        from acquired
-    ),
-
-    final as (select distinct
+    select distinct
         concat(case when platform_name = 'hboNow' then 0
                         else 1 end, '-', f.match_id) as match_id_platform
-        , max(case when cost is null then -1 else cost end) as content_cost
-    from {database}.{schema}.title_retail_funnel_metrics as f
-    left join table_union as t
-        on f.match_id = t.match_id
+        , ifnull(max(content_cost),-1) as content_cost
+    from  {database}.{schema}.title_retail_funnel_metrics as f
+    left join cost_base as c
+        on f.match_id = c.match_id
     group by 1
-    order by 1)
-
-    select
-        match_id_platform
-        , case
-            when match_id_platform = '1-GX9KHPw1OIMPCJgEAAAAD' then 200
-            when match_id_platform = '1-GYA79hQZbUsI3gQEAAAB0' then 50
-            when match_id_platform = '1-GYBmsKA4FaUnDdQEAAAAj' then 25
-            when match_id_platform = '1-GYFEzmwNES16GkQEAAAAC' then 165
-            when match_id_platform = '1-GYDAnZgCFQ8IJpQEAAAAN' then 70
-            else content_cost
-            end as content_cost
-    from final
     order by content_cost desc
 ) file_format = (type='csv' compression = 'NONE') single = true OVERWRITE = TRUE header = TRUE;

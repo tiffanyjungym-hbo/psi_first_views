9154c486c170941239e67923d2fa660c61bf56e4
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-09-08 10:45:14 -0400

    Dev prod cost (#386)
    * product_budget_test
    
    * production budget sql hotfix

--- Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_production_budget_imdb.sql
+++ Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_production_budget_imdb.sql
@@ -8,10 +8,9 @@ FROM (
                         then ifnull(asset.season_number, 1)
                         else asset.season_number
                             end as season_number_adj
-            , coalesce(concat(series_title_long, ' S', season_number_adj), asset_title_long) as title_name
             , coalesce(concat(asset.series_id, '-', season_number_adj), asset.viewable_id) as match_id
-            , content_category
-            , amount/1000000 as budget
+            , asset.viewable_id
+            , max(amount/1000000) as budget
         from enterprise_data.catalog.imdb_boxoffice_title_budget as b
         join enterprise_data.catalog.wm_catalog as c
             on b.title_id = c.imdb_title_id
@@ -22,39 +21,41 @@ FROM (
             and content_category is not null
             and title_name is not null
             and first_offered_date_max is not null
+            and asset.viewable_id is not null
+        -- group by in viewable_id level since same viewable_id could have more than 1 imdb id
+        group by 1, 2, 3
     ),
 
-    final as (
+    total_budget as (
         select distinct
-            f.title_name
-            , f.match_id
-            , f.content_category
-            , earliest_offered_timestamp
+              title_name
+            , concat(case when f.platform_name = 'hboNow' then 0
+                    else 1 end, '-', f.match_id) as match_id_platform
+            --, content_category
+            --, earliest_offered_timestamp
             , total_production_budget
         from (
                 select
-                    title_name
-                    , match_id
-                    , content_category
+                      match_id
                     , sum(budget) as total_production_budget
                 from base
-                group by 1,2,3
+                group by 1
             )  as b
         right join {database}.{schema}.title_retail_funnel_metrics as f
             on b.match_id = f.match_id
         where 1=1
-        order by earliest_offered_timestamp desc
-    )
+    ),
 
-    select
-        title_name
-        , match_id
-        --, content_category
-        --, earliest_offered_timestamp
-        , case when total_production_budget is null then -1
-            else total_production_budget end as total_production_budget_imdb
+    final as (
+        select
+            match_id_platform
+            , case when total_production_budget is null then -1
+                else total_production_budget end as total_production_budget_imdb
+        from total_budget)
+
+    select *
     from final
-    order by total_production_budget desc
+    order by total_production_budget_imdb desc
 ) file_format = (type = 'csv' 
         compression = 'NONE' 
         field_optionally_enclosed_by = '"') single = true OVERWRITE = TRUE header = TRUE;

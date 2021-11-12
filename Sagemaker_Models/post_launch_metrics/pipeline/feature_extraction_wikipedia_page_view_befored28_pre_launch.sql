ce5f56fd87bb9fbf0971b6db69bd3eea9223c33d
meichenghbo <63314617+meichenghbo@users.noreply.github.com>
2021-11-12 16:58:05 -0500

    Dev wiki view pipeline (#503)
    * hotfix
    
    * wikipedia page view pipeline
    
    * hotfix
    
    * yml file update
    
    * hotfix
    
    * hotfix
    
    * hotfix
    
    * hotfix
    
    * hotfix
    
    * hotfix to befored28 query

--- Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_wikipedia_page_view_befored28_pre_launch.sql
+++ Sagemaker_Models/post_launch_metrics/pipeline/feature_extraction_wikipedia_page_view_befored28_pre_launch.sql
@@ -2,25 +2,24 @@ USE SCHEMA workspace;
 
 COPY INTO {stage}/input_percent_view/wiki_view_feature_before28.csv
 FROM (
-
     with tmp as (
-    select
-          match_id_platform
-        , sum(total_page_views) as wiki_befored28_total
-    from {database}.{schema}.title_wikipedia_page_view as v
-    where v.days_since_first_offered < -27
-    group by 1
-    )
+        select
+            match_id_platform
+            , sum(total_page_views) as wiki_befored28_total
+        from {database}.{schema}.title_wikipedia_page_view as v
+        where v.days_since_first_offered < -27
+        group by 1
+        )
 
-select distinct
-    title_name
-    , match_id_platform
-    , ifnull(wiki_befored28_total, -1) as wiki_befored28_total
-from tmp
-left join  {database}.{schema}.title_retail_funnel_metrics as t
-     on concat(case when t.platform_name = 'hboNow' then 0
-        else 1 end, '-', t.match_id) = tmp.match_id_platform
-order by wiki_befored28_total desc
+    select distinct
+        concat(case when t.platform_name = 'hboNow' then 0
+            else 1 end, '-', t.match_id) as match_id_platform
+        , ifnull(wiki_befored28_total, -1) as wiki_befored28_total
+    from tmp
+    right join {database}.{schema}.title_retail_funnel_metrics as t
+        on concat(case when t.platform_name = 'hboNow' then 0
+            else 1 end, '-', t.match_id) = tmp.match_id_platform
+    order by wiki_befored28_total desc
 
 ) file_format = (type='csv' compression = 'NONE') single = true OVERWRITE = TRUE header = TRUE;
 
